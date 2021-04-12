eaa7f371d3fd7c8165d261297ca32d8543a91733
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2021-04-13 00:29:10 +0800

    Finish the sidebar Header

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,16 +1,86 @@
 import React from 'react';
 import styled from 'styled-components';
+import { 
+    Avatar, IconButton, Button
+} from "@material-ui/core";
+
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
 
 function Sidebar() {
     return (
         <Container>
-            <h1>Sidebar</h1>
+            <Header>
+                <UserAvatar />
+
+                <IconContainer>
+                    <IconButton>
+                        <ChatIcon />
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconContainer>
+            </Header>
+
+            <Search>
+                <SearchIcon />
+                <SearchInput  placeholder = "Search in chats" />
+            </Search>
+
+            <SidebarButton>Start a new chat</SidebarButton>
         </Container>
     )
 }
 
 export default Sidebar;
 
-const Container = styled.div`
 
+const Container = styled.div``;
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: #fff;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
 `;
+
+const UserAvatar = styled(Avatar)`
+    transition: all ease .3s;
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconContainer = styled.div``;
+
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+    outline: none;
+    border: none;
+    flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+    width: 100%;
+
+    &&& {
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+    }
+`;
+
