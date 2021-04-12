f8e2d7b38d3fd2ae38fd368a595d4ca04b8b8a5f
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2021-04-13 00:01:28 +0800

    Adding Material Ui and styled Components

---
+++ components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Sidebar() {
+    return (
+        <Container>
+            <h1>Sidebar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div`
+
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>Let's build Whatsapp</h1>
+     <Sidebar />
      
     </div>
   )
