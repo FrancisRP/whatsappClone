958a3a2b313473f2c0766df0cbc656e26e5f2fb3
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2021-04-14 01:29:29 +0800

    Adding users name on the sidebar

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core"
+import getRecipientEmail from '../utils/getRecipientEmail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase';
 
 function Chat({ id, users }) {
+    const [user] = useAuthState(auth);
+    const recipientEmail = getRecipientEmail(users, user);
+
     return (
         <Container>
             <UserAvatar />
-            <p>Recipient Email</p>
+            <p>{recipientEmail}</p>
         </Container>
     )
 }
