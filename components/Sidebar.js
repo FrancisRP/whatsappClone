6a7c5c6e7c0568b5e8c69d5976481f476317d300
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2021-04-14 00:49:21 +0800

    Adding validation for same email in chats

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,28 +3,38 @@ import styled from 'styled-components';
 import { 
     Avatar, IconButton, Button
 } from "@material-ui/core";
-
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore"
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 import * as EmailValidator from "email-validator";
-import { auth } from "../firebase";
+
 
 
 function Sidebar() {
+
+    const [user] = useAuthState(auth);
+    const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
+    const [chatsSnapshot] = useCollection(userChatRef);
 
     const createChat = () => {
         const input = prompt('Please enter an email address for the user you wish to chat with');
 
         if( !input ) return null;
 
-        if ( EmailValidator.validate(input) ) {
+        if ( EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email ) {
             db.collection('chats').add({
-                
+                users: [user.email, input]
             })
         }
 
     };
+
+    const chatAlreadyExists = (recipientEmail) => {
+        !!chatsSnapshot?.docs.find(chat => chat.data().users.find((user) => user === recipientEmail)?.length > 0 ); 
+    }
 
     return (
         <Container>
@@ -41,10 +51,10 @@ function Sidebar() {
 
             <Search>
                 <SearchIcon />
-                <SearchInput onClick={createChat} placeholder = "Search in chats" />
+                <SearchInput  placeholder = "Search in chats" />
             </Search>
 
-            <SidebarButton>Start a new chat</SidebarButton>
+            <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
         </Container>
     )
 }
